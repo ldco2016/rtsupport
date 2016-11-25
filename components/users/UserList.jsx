fa5dcc2b12c08a3389a29897253960ac76bb3b11
ldco2016 <renaissance.scholar2012@gmail.com>
2016-11-24 21:15:57 -0500

    Troubleshooting User and Message Components and developed Server

---
+++ components/users/UserList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import User from './User.jsx';
+
+class UserList extends React.Component{
+	render(){
+		return (
+			<ul>{
+				this.props.users.map(user =>{
+					return (
+						<User key={user.id} user={user} />
+					)
+				})
+			}</ul>
+		)
+	}
+}
+
+UserList.PropTypes = {
+	users: React.PropTypes.array.isRequired
+}
+
+export default UserList
